afe4c90d5f168a15cfc24237dd494c78af36d67d
Any-Image <145411316+Any-Image@users.noreply.github.com>
2023-09-19 10:09:21 +0800

    Add files via upload

---
+++ 泛影云前端代码/public/config/backendapi.js
@@ -0,0 +1,9 @@
+window.g = {
+    // baseURL: "http://127.0.0.1",
+    baseURL: window.location.href,
+    edgeUrlStr2: ["172.21.21.105","172.21.21.106","172.21.21.107","172.21.21.108"],
+    defaultWebPort: ":30081",//用于边缘端访问泛影云主页
+    dcm4cheePort: ":31080",//患者上传影像文件
+    backendPort: ":30063",//后端端口
+    airPort: ":30082"//查看影像
+}
